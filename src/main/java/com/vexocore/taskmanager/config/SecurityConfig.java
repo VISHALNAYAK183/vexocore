1196c8634a4323ef5772d1957f2b898ef7f64cda
vishal <vishunyk108@gmail.com>
2025-09-02 11:08:08 +0530

    Cors Configuration

--- src/main/java/com/vexocore/taskmanager/config/SecurityConfig.java
+++ src/main/java/com/vexocore/taskmanager/config/SecurityConfig.java
@@ -1,9 +1,14 @@
 package com.vexocore.taskmanager.config;
 
+import java.util.*;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -14,5 +19,16 @@ public class SecurityConfig {
                 .anyRequest().permitAll() 
             );
         return http.build();
+    }
+     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
